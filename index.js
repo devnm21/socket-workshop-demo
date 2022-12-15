4bbb8b554606862344d7a5c81b5be41b9e8f00d0
Devakumar NM <pydev21@gmail.com>
2022-12-15 23:10:51 +0530

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http')
+const socketIO = require('socket.io')
+const os = require('os')
+
+const fs = require('fs')
+const server = http.createServer(function (req, res) {
+    fs.readFile(__dirname + '/public/index.html', function(err, data) {
+        res.writeHead(200)
+        res.end(data)
+    })
+}) 
+
+const io = socketIO(server)
+io.on('connection', function (socket) {
+    socket.on('add-message', data => {
+        io.emit('new-message', data)
+    })
+})
+
+
+server.listen(5000)
